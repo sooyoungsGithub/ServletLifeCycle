33177ee7331fc6abd3d1f5890c8c167b683db37b
sooyoungs0120@gmail.com <sooyoungs0120@gmail.com>
2019-01-15 21:25:07 +0900

    ServletLCEx

--- src/lifeCycle/LCEx.java
+++ src/lifeCycle/LCEx.java
@@ -10,40 +10,57 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LCEx
- */
+
 @WebServlet("/lifecycle")
 public class LCEx extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public LCEx() {
         System.out.println("--- Create LCEx Servlet ---");
     }
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
+	
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		
 		System.out.println("---- Init() ---");
 	}
 
-	/**
-	 * @see Servlet#destroy()
-	 */
+	
 	public void destroy() {
-		// TODO Auto-generated method stub
-		System.out.println("--- If you update file or Dead Server --> Start destroy() ---");
+		
+		System.out.println("--- If you update application or Dead Server --> Start destroy() ---");
 	}
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("--- service()! ---");
+		
+		System.out.println("--- service()! --> get, post ---");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>formEx</title></head>");
+		out.println("<body>");
+		out.println("<form method='post' action='/ServleLifeCycle/lifecycle'>");
+		out.println("Name : <input type = 'text' name = 'name'>");
+		out.println("<input type = 'submit' value = 'post!'>");
+		out.println("</form>");
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		out.println("<h1> Welcome " + name + "!!! </h1>");
+		out.close();
+		
 	}
 
 }
